0c936706de8e924621e8c4cb4686e733888985db
khian calderon <ckhianvictory@gmail.com>
2025-11-16 19:48:28 +0800

    Added database default export

--- utils/db.ts
+++ utils/db.ts
@@ -4,3 +4,5 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL!,
     ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 })
+
+export default pool;
